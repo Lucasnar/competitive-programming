615d738249a0be8d176949f420813b8da5b0c0c8
Lucas Narciso <lucasnar@gmail.com>
2017-05-02 09:20:54 -0300

    Solve URI 1012

---
+++ URI/1012/1012.cpp
@@ -0,0 +1,28 @@
+#include<cstdio>
+#include<algorithm>
+#include<stdlib.h>
+#include<vector>
+#include<iostream>
+#include<map>
+#include<queue>
+#include<cstdlib>
+#include<cstring>
+#include<string>
+#include<set>
+
+using namespace std;
+
+const int inf = 0x3f3f3f3f;
+const int maxn = 100100; // 10 ^ 5
+
+typedef long long int huge;
+
+int main(){
+  double a, b, c;
+
+  scanf(" %lf %lf %lf", &a, &b, &c);
+
+  printf("TRIANGULO: %.3lf\nCIRCULO: %.3lf\nTRAPEZIO: %.3lf\nQUADRADO: %.3lf\nRETANGULO: %.3lf\n", (a * c)/2, 3.14159 * c * c, ((a + b) * c)/2, b * b, a * b);
+
+  return 0;
+}
