fe9574c3c9a4e491c7dcf03e0c7b150cffc2071f
Lucas Narciso <lucasnar@gmail.com>
2017-05-02 10:11:43 -0300

    Solve URI's 1016

---
+++ URI/1016/1016.cpp
@@ -0,0 +1,28 @@
+#include<cstdio>
+#include<algorithm>
+#include<stdlib.h>
+#include<vector>
+#include<iostream>
+#include<map>
+#include<queue>
+#include<cstdlib>
+#include<cstring>
+#include<string>
+#include<set>
+
+using namespace std;
+
+const int inf = 0x3f3f3f3f;
+const int maxn = 100100; // 10 ^ 5
+
+typedef long long int huge;
+
+int main(){
+  int n;
+
+  scanf("%d\n", &n);
+
+  printf("%d minutos\n", n*2);
+
+  return 0;
+}
