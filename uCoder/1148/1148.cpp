a87eb0c93f3eff2a91278bd16729d67c79670a0c
Lucas Narciso <lucasnar@gmail.com>
2017-07-03 00:25:34 -0300

    Solve uCoder's 1148
    The previous solution attempt was ignored and this one was started
    all the way from the beginning.

--- uCoder/1148/1148.cpp
+++ uCoder/1148/1148.cpp
@@ -1,89 +1,83 @@
-// NAO RESOLVIDO
-
 #include<cstdio>
-#include<bits/stdc++.h>
+#include<algorithm>
+#include<stdlib.h>
 #include<vector>
+#include<iostream>
+#include<map>
+#include<queue>
+#include<cstdlib>
+#include<cstring>
+#include<string>
+#include<set>
 
 using namespace std;
 
-const int maxn = 10010;
-vector<int> castelo[maxn];
-vector<int> chaves_atuais;
-int visitado[maxn];
-int chaves[maxn];
-int alcanca[maxn];
+const int inf = 0x3f3f3f3f;
+const int maxn = 1024 * 16;
+
+typedef pair<int, int> pii;
+
+vector<int> adj[maxn];
+vector<int> key[maxn];
+set<int> keychain;
+set<int> connected;
+int visited[maxn];
+
+typedef long long int huge;
 
 int main(){
-    int n, m, a, b, c;
+  int n, m, u, v;
 
-    while(scanf(" %d %d", &n, &m) == 2){
-
-        for(int i=0; i<m;++i)
-                castelo[i].clear();
-        chaves_atuais.clear();
-
-        for(int i = 1; i<n; ++i){
-            chaves[i] = 0;
-            visitado[i] = 0;
-            alcanca[i] = 0;
-        }
-
-        for(int i = 0; i<m; ++i){
-            scanf(" %d %d", &a, &b);
-            --a, --b;
-            castelo[a].push_back(b);
-            castelo[b].push_back(a);
-        }
-
-        for(int i = 1; i<n; ++i){
-            scanf(" %d", &c);
-            --c;
-            chaves[c] = i;
-        }
-
-        chaves_atuais.push_back(chaves[0]);
-        alcanca[0] = 1;
-        visitado[0] = 1;
-
-        for(int i = 0; i<n; ++i){
-
-                for(int j = 0, t = chaves_atuais.size(); j<t; ++j)
-                    if (chaves_atuais[j] == i){
-                        chaves_atuais.push_back(chaves[i]);
-                        visitado[i] = 1;
-                        break;
-                    }
-
-                for(int j = 0, t = castelo[i].size(); j<t; ++j){
-                    //printf("visitado[castelo[i][j]] = visitado[castelo[%d][%d]] = visitado[%d] = %d\n", i, j, castelo[i][j], visitado[castelo[i][j]]);
-                    if (alcanca[i] && !visitado[castelo[i][j]] && find(chaves_atuais.begin(), chaves_atuais.end(), castelo[i][j]) != chaves_atuais.end()){
-                        alcanca[castelo[i][j]] = 1;
-                        chaves_atuais.push_back(chaves[castelo[i][j]]);
-                        //printf("alcacei o %d\n", castelo[i][j]+1);
-                    }
-                }
-
-                //for(int j = 0, t = castelo[i].size(); j<t; ++j){
-                    //printf("visitado[castelo[i][j]] = visitado[castelo[%d][%d]] = visitado[%d] = %d\n", i, j, castelo[i][j], visitado[castelo[i][j]]);
-                    //if (alcanca[i] && !visitado[castelo[i][j]] && find(chaves_atuais.begin(), chaves_atuais.end(), castelo[i][j]) != chaves_atuais.end()){
-                        //alcanca[castelo[i][j]] = 1;
-                        //printf("alcacei o %d\n", castelo[i][j]+1);
-                    //}
-                //}
-
-       }
-
-        for(int i = 0; i<n; ++i)
-            if(!visitado[i]){
-                printf("nao");
-                break;
-            }
-            else if( i == n-1)
-                printf("sim");
-        printf("\n");
-        
-
+  while(scanf(" %d %d", &n, &m) == 2){
+    for(int i = 0; i<n; ++i){
+      key[i].clear();
+      adj[i].clear();
+      visited[i] = 0;
+    }
+    connected.clear();
+    keychain.clear();
+    //printf(" %d %d \n", n, m);
+    for(int i = 0; i<m; ++i){
+      scanf(" %d %d", &u, &v);
+      adj[--u].push_back(--v);
+      adj[v].push_back(u);
     }
 
-    return 0;
+    for(int i = 1; i<n; ++i){
+      scanf(" %d", &u);
+      key[--u].push_back(i);
+    }
+
+    connected.insert(0);
+    visited[0] = 0;
+    int cur = 0, last_cur = 0;
+    while(!connected.empty()){
+      for(int i = 0, size = adj[cur].size(); i<size; ++i)
+        if(!visited[adj[cur][i]]) connected.insert(adj[cur][i]);
+      for(int i = 0, size = key[cur].size(); i<size; ++i)
+        keychain.insert(key[cur][i]);
+      auto it = find(connected.begin(), connected.end(), cur);
+      if(it != connected.end()) connected.erase(it);
+      it = find(keychain.begin(), keychain.end(), cur);
+      if(it != keychain.end()) keychain.erase(it);
+      visited[cur] = 1;
+      last_cur = cur;
+      for(auto cit : connected){
+        if(find(keychain.begin(), keychain.end(), cit) != keychain.end()){
+          cur = cit;
+          goto there;
+        }
+      }
+there:
+      if(cur == last_cur) break;
+    }
+
+    int win = 1;
+    for(int i = 0; i<n; ++i)
+      if(!visited[i])
+        win = 0;
+    printf("%s\n", win? "sim" : "nao");
+  }
+
+  return 0;
 }
