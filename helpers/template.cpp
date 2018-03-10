f376a8219205d622c4c4d566b96cb6f073dd3a50
Lucas Narciso <lucasnar@gmail.com>
2018-03-10 19:07:47 -0300

    Adds helpers directory
    - gimme.sh <problem-name> will give you a <problem-name> directory with
    <problem-name>.cpp (based on template.cpp), input, output and the
    Makefile properly edited to compile the <problem-name>.cpp, run against
    the input file and compare its output with the output file.

---
+++ helpers/template.cpp
@@ -0,0 +1,22 @@
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+#include<iostream>
+#include<map>
+#include<queue>
+#include<cstdlib>
+#include<cstring>
+#include<string>
+#include<set>
+
+using namespace std;
+
+const int inf = 0x3f3f3f3f;
+const int maxn = 1024 * 128; // 10 ^ 5
+
+typedef long long int huge;
+
+int main(){
+
+  return 0;
+}
