0c8ac507c32f6ab9e73f27c5387cf4b0f377b0c7
Lucas Narciso <lucasnar@gmail.com>
2018-04-10 22:34:06 -0300

    Add some Google Code Jam 2017 problems

---
+++ google-code-jam/2017/gimme.sh
@@ -0,0 +1,9 @@
+for folder in "$@"
+do
+    mkdir "$folder"
+    touch "$folder"/input
+    touch "$folder"/output
+    echo "file=$folder" > $folder/Makefile
+    cat Makefile >> "$folder"/Makefile 
+    cat template.cpp > "$folder"/"$folder".cpp
+done
